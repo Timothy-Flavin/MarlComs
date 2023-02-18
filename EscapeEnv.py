e3ed6b96a7cf31554b33c1220f1bf4ec67b8ea90
Timothy Flavin <BinaryOwlizard@gmail.com>
2023-02-18 14:45:49 -0600

    Changed code language to chaser instead of zombie

--- EscapeEnv.py
+++ EscapeEnv.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 
 class survivor:
-  def __init__(self, id, x, y, n_gens, n_players, n_zoms, view_range = 2):
+  def __init__(self, id, x, y, n_gens, n_players, n_chasers, view_range = 2):
     self.id = id
     self.x = x
     self.y = y
@@ -12,7 +12,7 @@ class survivor:
     # gen dx = 0 dy = 0 and recency is -1 if not seen or completed and completed is 0 or 1
     self.players_info = np.zeros(shape=(n_players,5)) # [player id] [x, y, recency, alive, repairing]
     self.players_info[:,3] = 1
-    self.zombie_info = np.zeros(shape=(n_zoms,3)) # [zom id] [x, y, recency]
+    self.chaser_info = np.zeros(shape=(n_chasers,3)) # [chaser id] [x, y, recency]
     self.view_range = view_range
     self.repairing = 0
     self.took_actions = np.zeros(n_players)
@@ -24,25 +24,25 @@ class survivor:
       p = self.players_info[i]
       st += f"  player [{i}] info: x: {p[0]}, y: {p[1]}, recency: {p[2]}, alive: {p[3]}\n"
     st+="\n"
-    for i in self.zombie_info.shape[0]:
-      z = self.zombie_info[i]
-      st += f"  zombie [{i}] info: x: {z[0]}, y: {z[1]}, recency: {z[2]}\n"
+    for i in self.chaser_info.shape[0]:
+      z = self.chaser_info[i]
+      st += f"  chaser [{i}] info: x: {z[0]}, y: {z[1]}, recency: {z[2]}\n"
     st +="\n"
     for i in self.gens_info.shape[0]:
       g = self.gens_info[i]
       st += f"  gen [{i}] info: x: {g[0]}, y: {g[1]}, recency: {g[2]}, completed: {g[3]}\n"
     st +="\n"
 
-class zombie:
+class chaser:
   def __init__(self, id, x, y, view_range = 2, n_players=2):
     self.id = id
     self.x = x
     self.y = y
     self.view_range = view_range
-    # recency is an int where zero means the zombie can't see that player
-    # the int is decrimented towards zero each turn and the zombie can
+    # recency is an int where zero means the chaser can't see that player
+    # the int is decrimented towards zero each turn and the chaser can
     # only act if the int is greater than zero. x,y will not update unless
-    # a player is in the zombie's vision or if they communicate
+    # a player is in the chaser's vision or if they communicate
     self.player_locs = np.zeros((n_players, 3)) # player [x, y, recency]
 
 class generator: 
@@ -53,11 +53,11 @@ class generator:
     self.completed = 0
 
 class env:
-  def __init__(self, map_size = [10,10], n_players = 2, n_zoms=1, gen_locs = np.array([[4,4], [1,8], [8,8]]), player_start_locs = np.array([[8,1], [7,2]]), i_decay = 0.2,  gen_turns=2, max_steps = -1, flatten=True):
+  def __init__(self, map_size = [10,10], n_players = 2, n_chasers=1, gen_locs = np.array([[4,4], [1,8], [8,8]]), player_start_locs = np.array([[8,1], [7,2]]), i_decay = 0.2,  gen_turns=2, max_steps = -1, flatten=True):
     self.n_players = n_players
     self.player_ids = np.arange(n_players)
     #print(f"Player id's {self.player_ids}")
-    self.n_zoms = n_zoms
+    self.n_chasers = n_chasers
     self.map_size = np.array(map_size)
     self.gen_locs = gen_locs
     self.n_gens = len(gen_locs)
@@ -79,11 +79,11 @@ class env:
     for p in self.players:
       # if the player can see an entity, set the x and y and recency to 1
       # otherwise, decay the recency
-      for z in self.zombies:
+      for z in self.chasers:
         if self.viewable_by(z.x,z.y, p):
-          p.zombie_info[z.id] = np.array([z.x,z.y,1.0])
+          p.chaser_info[z.id] = np.array([z.x,z.y,1.0])
         else:
-          p.zombie_info[z.id] = np.array([p.zombie_info[z.id,0],p.zombie_info[z.id,1],max(p.zombie_info[z.id,2]-self.i_decay,0)])
+          p.chaser_info[z.id] = np.array([p.chaser_info[z.id,0],p.chaser_info[z.id,1],max(p.chaser_info[z.id,2]-self.i_decay,0)])
 
       for op in self.players:
         if self.viewable_by(op.x,op.y,p):
@@ -103,8 +103,8 @@ class env:
             rec = 1
           p.gens_info[g.id] = np.array([p.gens_info[g.id,0],p.gens_info[g.id,1], rec, p.gens_info[g.id,3]])
 
-  def update_zombie_info(self):
-    for z in self.zombies:
+  def update_chaser_info(self):
+    for z in self.chasers:
       for p in self.players:
         if self.viewable_by(p.x,p.y,z):
           z.player_locs[p.id] = np.array([p.x,p.y,1.0])
@@ -125,12 +125,12 @@ class env:
     
     self.players = []
     for i in range(len(self.player_start_locs)):
-      self.players.append(survivor(id=i, x=self.player_start_locs[i,0], y=self.player_start_locs[i,1], n_gens = self.n_gens, n_players = self.n_players, n_zoms = self.n_zoms))
+      self.players.append(survivor(id=i, x=self.player_start_locs[i,0], y=self.player_start_locs[i,1], n_gens = self.n_gens, n_players = self.n_players, n_chasers = self.n_chasers))
     self.gens = []
     for i in range(self.n_gens):
       self.gens.append(generator(i,self.gen_locs[i,0], self.gen_locs[i,1]))
-    self.zombies = []
-    for i in range(self.n_zoms):
+    self.chasers = []
+    for i in range(self.n_chasers):
       onplayer = True
       x,y = 0,0
       while onplayer:
@@ -140,7 +140,7 @@ class env:
         for p in self.players:
           if x == p.x and y == p.y:
             onplayer = True
-      self.zombies.append(zombie(i,x,y,3))
+      self.chasers.append(chaser(i,x,y,3))
     self.update_player_info()
     return self.observe()
 
@@ -166,9 +166,9 @@ class env:
     for p in self.players:
       if player is None or self.viewable_by(p.x,p.y, player):
         zmap[p.x][p.y] = f"P{p.id}"
-    for z in self.zombies:
+    for z in self.chasers:
       if player is None or self.viewable_by(z.x,z.y, player):
-        zmap[z.x][z.y] = f"Z{z.id}"
+        zmap[z.x][z.y] = f"C{z.id}"
     
     if player is not None:
       for y in range(player.y-player.view_range, player.y+player.view_range+1):
@@ -183,8 +183,8 @@ class env:
         if p_inf[2] > 0 and p_inf[2] < 1:
           zmap[int(p_inf[0])][int(p_inf[1])] = f"p{p}"
         
-      for z in range(player.zombie_info.shape[0]):
-        z_inf = player.zombie_info[z]
+      for z in range(player.chaser_info.shape[0]):
+        z_inf = player.chaser_info[z]
         if z_inf[2]>0 and z_inf[2]<1:
           zmap[int(z_inf[0])][int(z_inf[1])] = f"z{z}"
       
@@ -220,13 +220,13 @@ class env:
     the player can see in a x+1+x by x+1+x box 
     
     obs format: np array(4,mapsize[0],mapsize[1])
-                (gens, players, zombies, me) 
+                (gens, players, chasers, me) 
     where 
     gens: np float array(shape = map_size) 
           0 is a space not occupied by an unfinished 
           gen and > 0 is occupied
     players: same thing but players, 0 is dead
-    zombies: same thing but zombies 
+    chasers: same thing but chasers 
     self: location of myself
 
     and another array of misc information
@@ -254,7 +254,7 @@ class env:
       if p[3] > 0:
         players_alive +=1
       obs1[1,int(p[1]),int(p[0])] = p_status
-    for z in player.zombie_info:
+    for z in player.chaser_info:
       obs1[2,int(z[1]),int(z[0])] = z[2]
     obs1[3,int(x),int(y)] = 1
 
@@ -313,16 +313,16 @@ class env:
     elif dir==3:
       x-=1
     
-    for z in self.zombies:
+    for z in self.chasers:
       if x==z.x and y==z.y:
         self.players[id].alive = 0
-        # remove self from zombie's list
+        # remove self from chaser's list
         z.player_locs[id,2] = 0
         #self.players[id].num_alive -=1
        
         reward -= 1
         community_reward -=0.5
-        print(f"eaten by zombie r {reward}, cr {community_reward}")
+        print(f"tagged by chaser r {reward}, cr {community_reward}")
         return reward, community_reward
 
     if not self.oob(x,y):
@@ -334,7 +334,7 @@ class env:
         community_reward += 2.5
     return reward, community_reward
 
-  def zombie_move(self, z):
+  def chaser_move(self, z):
     rewards = np.zeros(len(self.players))
     community_rewards = np.zeros(len(self.players))
     max_dist = self.map_size[0] * self.map_size[1]
@@ -348,7 +348,7 @@ class env:
           max_dist = p_dist
           target = p
           if p_dist == 0:
-            print(f"zombie got player {p}")
+            print(f"chaser got player {p}")
             self.players[p].alive=0
             z.player_locs[p,2] = 0
             rewards[p]-=1
@@ -393,7 +393,7 @@ class env:
       dx = self.players[target].x - z.x
       dy = self.players[target].y - z.y
       if dx==0 and dy==0 and max_dist>0:
-        print(f"zombie got player {target} after moving")
+        print(f"chaser got player {target} after moving")
         self.players[target].alive=0
         z.player_locs[target,2] = 0
         rewards[target]-=1
@@ -456,11 +456,11 @@ class env:
         rewards[agent] += r
         self.community_rewards[agent] += cr
       
-    for z in self.zombies:
-      rs, crs = self.zombie_move(z)
+    for z in self.chasers:
+      rs, crs = self.chaser_move(z)
       rewards += rs
       self.community_rewards += crs
-    self.update_zombie_info()
+    self.update_chaser_info()
     self.update_player_info()
     
 
@@ -473,4 +473,4 @@ class env:
       self.took_actions = np.ones(self.n_players)
     if self.max_steps > 0 and self.steps >= self.max_steps:
       truncated = True
-    return observations, rewards, self.done, truncated, {"n_players": self.n_players, "n_gens": self.n_gens, "gens_active": self.gens_active, "n_zombies": self.n_zoms, "step":self.steps, "max_steps": self.map_size, "map_size":self.map_size}
+    return observations, rewards, self.done, truncated, {"n_players": self.n_players, "n_gens": self.n_gens, "gens_active": self.gens_active, "n_chasers": self.n_chasers, "step":self.steps, "max_steps": self.map_size, "map_size":self.map_size}
